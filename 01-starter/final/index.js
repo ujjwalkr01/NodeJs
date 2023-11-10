3244a2d89e040ea14c171cd81d018b50296b5b6b
ujjwlkr01 <99142201+ujjwlkr01@users.noreply.github.com>
2023-11-10 11:00:15 +0530

    update

--- 01-starter/final/index.js
+++ 01-starter/final/index.js
@@ -1,5 +1,5 @@
 /*To run the js file using Node js the command--> node fileName* */
-
+/**To exit from the enviroment of nodejs--> .exit/ctrl+d */
 const hello = "hello world";
 console.log(hello);
 
